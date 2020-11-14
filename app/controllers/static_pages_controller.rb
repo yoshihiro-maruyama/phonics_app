fb5b6f176b743dee19deffd015d81732a2cc9dc1
Yoshihiro.Maruyama <y.maruyama0008@gmail.com>
2020-11-14 17:33:51 +0900

    made practice page's template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def practice
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
+  get '/practice', to: 'static_pages#practice'
 end
