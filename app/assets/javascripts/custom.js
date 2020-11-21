f832bfc5c77d2c10ee7e852e6eadc49eaa439852
Yoshihiro.Maruyama <y.maruyama0008@gmail.com>
2020-11-21 21:16:56 +0900

    move audio func to custom.js

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,13 @@
+function sample(){
+  document.getElementById("area1").innerText = "変更しました";
+}
+
+function audioPractice() {
+  var $elementReference = document.getElementById( "audio-1" );
+  $elementReference.play();
+}
+
+function audioAlphabet() {
+  var $elementReference = document.getElementById( "audio" );
+  $elementReference.play();
+}
